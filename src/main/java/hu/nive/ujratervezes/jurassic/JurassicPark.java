1bfc25b1161bc17024b187d942f553aca95b2ac6
morton2 <gab.marcell@gmail.com>
2021-04-29 12:45:08 +0200

    result

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,33 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e) {
+            System.err.println("Database not reachable.");
+        }
+        return connection;
+    }
+
+    public List<String> checkOverpopulation() {
+        List<String> name = new ArrayList<>();
+        String query = "SELECT breed FROM dinosaur WHERE expected <> actual  ORDER BY breed ASC";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                name.add(resultSet.getString(1));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return name;
+    }
 }
+
